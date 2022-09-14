9dc8568d110e35fe803eb2c10c413f3c01237d01
UmarShareefPk <76015950+UmarShareefPk@users.noreply.github.com>
2022-09-14 22:48:34 +0500

    Incident Listing half done

---
+++ Controllers/IncidentController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using IM.Data;
+using IM.Models;
+
+namespace MVCWeb.Controllers
+{
+    public class IncidentController : Controller
+    {
+
+        private readonly IIncidentService incidentService;
+        public IncidentController(IIncidentService _incidentService)
+        {
+            incidentService = _incidentService;
+        }
+
+        [HttpGet]
+        public ActionResult Listing()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> ListingPage([FromQuery] string pageSize, [FromQuery]  string pageNumber, [FromQuery] string search)
+        {
+            string token = User.Claims.Where(c => c.Type == "Token").FirstOrDefault().Value;
+            IncidentPages incidentPages = await incidentService.GetIncidentsWithPage(token, int.Parse(pageSize), int.Parse(pageNumber), search);
+            return Json(incidentPages);
+        }
+
+        // GET: IncidentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: IncidentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: IncidentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: IncidentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: IncidentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: IncidentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: IncidentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,3 +10,174 @@ $(document).ready(function () {
     });
 
 });
+
+
+function getStatusByCode(code) {
+    switch (code) {
+        case "N":
+            return "New";
+        case "I":
+            return "In Progress";
+        case "A":
+            return "Approved";
+        case "C":
+            return "Closed";
+        default:
+            return "Invalid";
+    }
+}
+
+function auto_grow(element) { // for textareas
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight) + "px";
+}
+
+function getUserNameById(userId) {
+    //let allUsers = JSON.parse(localStorage.getItem("allUsers"));
+    //let user = allUsers.find((user) => {
+    //    return user.Id === userId;
+    //});
+    //return user.FirstName + " " + user.LastName;
+    return "Umar Shareef";
+}
+
+function getAssingeesBySearch(search) {
+    let allAssignees = JSON.parse(localStorage.getItem("allUsers"));
+
+    let newList = [];
+    if (search !== "") {
+        newList = allAssignees.filter((assignee) => {
+            return (
+                assignee.FirstName.toUpperCase().startsWith(search.toUpperCase())
+                ||
+                assignee.LastName.toUpperCase().startsWith(search.toUpperCase())
+            );
+        });
+    }
+
+    if (newList !== undefined && newList.length !== 0) {
+        //check if there is atlease one assignee
+        newList = [].concat(newList);
+    } else {
+        //if search found nothing, show all assignees
+        newList = allAssignees.slice(0, allAssignees.length);
+    }
+
+    return newList;
+}
+
+function setDate(datetime) {
+    let date = new Date(datetime);
+
+    return (
+        (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear()
+        + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+    )
+}
+
+
+function getAllNotifications() {
+    let user = JSON.parse(localStorage.getItem("userLogin"));
+    let token = user.Token;
+    let userId = user.user.Id;
+
+    axios.defaults.headers = { 'Authorization': `Bearer ${token + ""}` };
+    const url = apiBaseUrl + "api/Users/UserNotifications" + "?userId=" + userId;
+    return axios.get(url);
+}
+
+function setNotificationStatus(id, isRead) {
+    let user = JSON.parse(localStorage.getItem("userLogin"));
+    let token = user.Token;
+
+    axios.defaults.headers = { 'Authorization': `Bearer ${token + ""}` };
+    const url = apiBaseUrl + "api/Users/UpdateIsRead" + "?notificationId=" + id + "&isRead=" + isRead;
+    axios.get(url)
+        .then((response) => {
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function updateHubId(hubId) {
+    let user = JSON.parse(localStorage.getItem("userLogin"));
+    let token = user.Token;
+    let userId = user.user.Id;
+
+    axios.defaults.headers = { 'Authorization': `Bearer ${token + ""}` };
+    const url = apiBaseUrl + "api/Users/UpdateHubId";
+    axios.post(url, {
+        HubId: hubId,
+        UserId: userId
+    }).then((response) => {
+        console.log("HubId updated");
+    })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function sendUpdateToSignalR(incidentId) {
+    let user = JSON.parse(localStorage.getItem("userLogin"));
+    let userId = user.user.Id;
+
+    let connection = new signalR.HubConnectionBuilder()
+        .withUrl(apiBaseUrl + "hubs/notifications")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.start()
+        .then(result => {
+            if (connection.connectionStarted) {
+                connection.send("SendIncidentUpdate", incidentId, userId);
+
+            } else {
+                alert("No connection to server yet.");
+            }
+        })
+        .catch(e => console.log('Connection failed: ', e));
+}
+
+function signlrListener() {
+
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl(apiBaseUrl + "hubs/notifications")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.start()
+        .then(result => {
+            console.log('Connected!');
+            let hubId = connection.connectionId;
+            updateHubId(hubId);
+
+            connection.on('UpdateNotifications', (incidentId) => {
+                console.log("Update Received from Hub : ", incidentId);
+                setnotifications();
+                toastr.success('An incident just updated. Please check your notifications.', 'Updates');
+            });
+        })
+        .catch(e => console.log('Connection failed: ', e));
+}
+
+
+function setToaster() { // UI alerts
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-full-width",
+        "preventDuplicates": true,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+}
